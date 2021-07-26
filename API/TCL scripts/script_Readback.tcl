8d45b8a47c89e066a2b69cfb4cc68093d195d261
Jacob Thibodeau <JATHI97@ulaval.ca>
2021-07-26 09:27:22 -0400

    API changes

--- API/TCL scripts/script_Readback.tcl
+++
@@ -1,9 +0,0 @@
-#
-#TCL scipt pour extraction du bitstream de la memoire de configuration
-#
-
-open_hw_manager
-connect_hw_server
-open_hw_target
-readback_hw_device [current_hw_device] -bin_file bitstream_readback.bin
-exit
